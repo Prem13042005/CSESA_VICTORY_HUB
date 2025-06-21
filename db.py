ede3ed56e5b73e2cf8ee69c0f88f0a4ea6a1a6d6
Prem Rajput <premrajrajput1345@gmail.com>
2025-06-21 16:08:12 +0530

    Fix: Updated requirements for Railway

--- db.py
+++ db.py
@@ -1,12 +1,13 @@
 import mysql.connector
+import os
 
 def get_db_connection():
     try:
         conn = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="WJ28@krhps",
-            database="csesa_db"
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME")
         )
         print("[OK] DB connection successful")
         return conn
