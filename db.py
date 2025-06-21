39da47f24814cf82f49ffcd423d6697cfcf37085
Prem Rajput <premrajrajput1345@gmail.com>
2025-06-21 16:53:21 +0530

    Fix: Updated requirements for Railway

--- db.py
+++ db.py
@@ -1,10 +1,13 @@
 import mysql.connector
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 def get_db_connection():
     try:
         conn = mysql.connector.connect(
             host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT"),
             user=os.getenv("DB_USER"),
             password=os.getenv("DB_PASSWORD"),
             database=os.getenv("DB_NAME")
